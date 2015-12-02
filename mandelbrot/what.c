65de5b4fe0d82353f44c4d128204a74a90a2363f
Brian Yu <brianyu8@gmail.com>
2015-12-02 10:14:29 -0500

    Non working mandelbrot

--- mandelbrot/what.c
+++ mandelbrot/what.c
@@ -23,7 +23,7 @@ void idlefunc()
 */
 void displayfunc()
 {
-   int x, y;
+   double x, y, x1, y1;
    //
    //
    glClear(GL_COLOR_BUFFER_BIT); // white
@@ -32,24 +32,7 @@ void displayfunc()
    {
       for( y = 0 ; y < N ; y++ )
       { 
-         double a = 0.0;
-         double b = 0.0;
-         double x0 = (((double)x)/N)*(2.5+2.5)-2.5;
-         double y0 = (((double)y)/N)*(1.5+1.5)-1.5;
-         int i = 0;
-         int max = 20;
-         int steps = 0;
-         for (i=0; i < max; i++) {
-            if (a*a + b*b < 2*2) {
-                double temp = x*x - y*y + x0;
-                b = 2*x*y + y0;
-                a = temp;
-                
-                if (i == max) {
-                    glColor3f( 0.0 , 0.0 , 0.0 ) ; // black
-                }
-            }
-         }
+         glColor3f( 0.0 , 0.0 , 0.0 ) ; // black
          glBegin(GL_POINTS);
          glVertex2f(x,y);
          glEnd();
@@ -59,27 +42,6 @@ void displayfunc()
    //         glColor3f( 0.0 , 0.0 , 0.0 ) ; // black 
    //
    glutSwapBuffers(); // single buffering... call glFlush();
-   
-//  For each pixel (Px, Py) on the screen, do:
-//  {
-//    x0 = scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.5, 1))
-//    y0 = scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1, 1))
-//    x = 0.0
-//    y = 0.0
-//    iteration = 0
-//    max_iteration = 1000
-//    while ( x*x + y*y < 2*2  AND  iteration < max_iteration )
-//    {
-//       xtemp = x*x - y*y + x0
-//       y = 2*x*y + y0
-//       x = xtemp
-//       iteration = iteration + 1
-//    }
-//    color = palette[iteration]
-//    plot(Px, Py, color)
-//  }
-
-
 }
 void reshapefunc(int wscr,int hscr)
 {
@@ -104,7 +66,6 @@ int main(int argc,char* argv[])
    glutCreateWindow("");
    glClearColor(1.0,1.0,1.0,0.0);
    glShadeModel(GL_SMOOTH);
-   
    
    
    glutIdleFunc(idlefunc);
