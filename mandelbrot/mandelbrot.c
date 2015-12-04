0a9a654f2debf1f7999976c3698bda42039090bf
Brian Yu <brianyu8@gmail.com>
2015-12-04 10:11:34 -0500

    MANDELBROOOOOO

--- mandelbrot/mandelbrot.c
+++ mandelbrot/mandelbrot.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <complex.h>
 // 
 #include <GL/glut.h>
 // 
-#define N     600
+#define N     1000
 // 
 // 
 // 
@@ -31,55 +32,36 @@ void displayfunc()
    for( x = 0 ; x < N ; x++ )
    {
       for( y = 0 ; y < N ; y++ )
-      { 
+      {
          double a = 0.0;
          double b = 0.0;
          double x0 = (((double)x)/N)*(2.5+2.5)-2.5;
          double y0 = (((double)y)/N)*(1.5+1.5)-1.5;
          int i = 0;
-         int max = 20;
+         int max = 100;
          int steps = 0;
          for (i=0; i < max; i++) {
-            if (a*a + b*b < 2*2) {
-                double temp = x*x - y*y + x0;
-                b = 2*x*y + y0;
-                a = temp;
-                
-                if (i == max) {
-                    glColor3f( 0.0 , 0.0 , 0.0 ) ; // black
-                }
+            if (a*a + b*b >= 2*2) {
+                break;
             }
+            else {
+               // double temp = (a+b)*(a+b) + x0;
+               double temp = a*a - b*b + x0;
+               b = 2*a*b + y0;
+               a = temp; 
+            }
+         }
+         
+         if (i == max) {
+            glColor3f( 0.0, 0.0, 0.0);
          }
          glBegin(GL_POINTS);
          glVertex2f(x,y);
          glEnd();
       }
    }
-   //
-   //         glColor3f( 0.0 , 0.0 , 0.0 ) ; // black 
-   //
    glutSwapBuffers(); // single buffering... call glFlush();
    
-//  For each pixel (Px, Py) on the screen, do:
-//  {
-//    x0 = scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.5, 1))
-//    y0 = scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1, 1))
-//    x = 0.0
-//    y = 0.0
-//    iteration = 0
-//    max_iteration = 1000
-//    while ( x*x + y*y < 2*2  AND  iteration < max_iteration )
-//    {
-//       xtemp = x*x - y*y + x0
-//       y = 2*x*y + y0
-//       x = xtemp
-//       iteration = iteration + 1
-//    }
-//    color = palette[iteration]
-//    plot(Px, Py, color)
-//  }
-
-
 }
 void reshapefunc(int wscr,int hscr)
 {
