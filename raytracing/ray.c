0e63f9ac6067536e4487e97c3db60b3480e7cf83
Brian Yu <brian@byu.io>
2016-02-25 23:58:30 -0500

    rayz

--- raytracing/ray.c
+++ raytracing/ray.c
@@ -30,11 +30,17 @@ typedef struct
 
 typedef struct
 {
-	triple c;
-	double r;
-	color h;
+    triple c;
+    double r;
+    color h;
 
 } sphere;
+
+typedef struct
+{
+    int key;
+    double val;
+} tuple;
 
 sphere a[NUMSPHERES] = {};
 
@@ -53,9 +59,8 @@ void diff( triple* t , triple u , triple v ) // t = u - v
    t->z = u.z - v.z ;
 }
 
-double magnitude(triple v)
-{
-	return sqrt(dotp(v,v));
+double magnitude(triple v) {
+    return sqrt(dotp(v,v));
 }
 
 void init()
@@ -93,69 +98,101 @@ void init()
    a[3].h.b =      0    ;
 }
 
-triple unitVector(double x, double y)
-{
+triple unitVector(double x, double y) {
     triple vector;
-	triple location = {x, y, 0};
-	diff(&vector, location, e);
-	double m = magnitude(vector);
-	vector.x = vector.x/m;
-	vector.y = vector.y/m;
-	vector.z = vector.z/m;
-	return vector;
+    triple location = {x, y, 0};
+    diff(&vector, location, e);
+    double m = magnitude(vector);
+    vector.x = vector.x/m;
+    vector.y = vector.y/m;
+    vector.z = vector.z/m;
+    return vector;
+}
+
+tuple minDist(triple e, triple r) {
+
+    
+    double min = 1.0/0;
+    int key = -1;
+
+    for (int i = 0; i < NUMSPHERES; i++) {
+        double b = 2*(dotp(e,r)-dotp(r, a[i].c));
+        double c = pow((e.x-a[i].c.x),2) + pow((e.y-a[i].c.y),2) + pow((e.z-a[i].c.z),2) - pow((a[i].r),2);
+        double d = b*b-4*c;
+        double T = (-b-sqrt(d)) / 2;
+        if (T < min && T > 0 && d>0) {
+            min = T;
+            key = i;
+        }
+    }
+    
+    tuple pair;
+    pair.key = key;
+    pair.val = min;
+    
+    return pair;
+    
 }
 
 int main(void)
 {
-	int rgb[H][W][3];
-	int y, x;
-	FILE* fout;
-	init();
-	for (y = 0; y < H; y++) {
-		for (x = 0; x < W; x++) {
-			double XSIZE = XMAX - XMIN;
-			double YSIZE = YMAX - YMIN;
-			double valx = XMIN + (((double)x) / W) * XSIZE;
-			double valy = YMIN + (((double)y) / H) * YSIZE;
-			triple r = unitVector(valx,valy);
+    int rgb[H][W][3];
+    int y, x;
+    FILE* fout;
+    init();
+    for (y = 0; y < H; y++) {
+        for (x = 0; x < W; x++) {
+            double XSIZE = XMAX - XMIN;
+            double YSIZE = YMAX - YMIN;
+            double valx = XMIN + (((double)x) / W) * XSIZE;
+            double valy = YMIN + (((double)y) / H) * YSIZE;
             
-        	double min = 1.0/0;
-        	int key = -1;
-        	for (int i = 0; i < NUMSPHERES; i++) {
-        		double b = 2*(dotp(e,r)-dotp(r, a[i].c));
-                double c = pow((e.x-a[i].c.x),2) + pow((e.y-a[i].c.y),2) + pow((e.z-a[i].c.z),2) - pow((a[i].r),2);
-                double d = b*b-4*c;
-        		double T = (-b-sqrt(d)) / 2;
-        		if (T < min && T > 0 && d>0) {
-        			min = T;
-        			key = i;
-        		}
-        	}
+            triple r = unitVector(valx,valy);
             
-			int i = key;
-			double T = min;
-			triple point = {e.x + r.x*T, e.y + r.y*T, e.z + r.z*T};
+            tuple pair;
+            pair = minDist(e, r);
+            int i = pair.key;
+            double T = pair.val;
+            
             int n = H-y-1;
-			if (i == -1) {
+            if (i == -1) {
                 rgb[n][x][0] = 178;
                 rgb[n][x][1] = 255;
                 rgb[n][x][2] = 255;
-			} else {
-				rgb[n][x][0] = (int)(a[i].h.r);
-				rgb[n][x][1] = (int)(a[i].h.g);
-				rgb[n][x][2] = (int)(a[i].h.b);
-			}
-		}
-	}
-	fout = fopen("output.ppm", "w");
-	fprintf(fout, "P3\n");
-	fprintf(fout, "%d %d\n",W,H);
-	fprintf(fout, "255\n");
-	for (y = 0; y < H; y++) {
-		for (x = 0; x < W; x++) {
-			 fprintf(fout, "%d %d %d\n", rgb[y][x][0], rgb[y][x][1], rgb[y][x][2]);
-		}
-	}
-	close(fout);
-	return 0;
+            } else {
+                rgb[n][x][0] = (int)(a[i].h.r);
+                rgb[n][x][1] = (int)(a[i].h.g);
+                rgb[n][x][2] = (int)(a[i].h.b);
+            }
+            
+            pair = minDist(g, r);
+            int il = pair.key;
+            double Tl = pair.val;
+            
+            triple v;
+            diff(&v, r, e);
+            
+            triple l;
+            diff(&l, g, a[i].c);
+            
+            double cosAngle = dotp(l, v)/(magnitude(l) * magnitude(v));
+            
+            if (cosAngle >= 0) {
+                rgb[n][x][0] = (int)(cosAngle * a[i].h.r);
+                rgb[n][x][1] = (int)(cosAngle * a[i].h.r);
+                rgb[n][x][2] = (int)(cosAngle * a[i].h.r);
+            }
+        }
+    }
+    fout = fopen("output.ppm", "w");
+    fprintf(fout, "P3\n");
+    fprintf(fout, "%d %d\n",W,H);
+    fprintf(fout, "255\n");
+    for (y = 0; y < H; y++) {
+        for (x = 0; x < W; x++) {
+             fprintf(fout, "%d %d %d\n", rgb[y][x][0], rgb[y][x][1], rgb[y][x][2]);
+        }
+    }
+    close(fout);
+    return 0;
 }
